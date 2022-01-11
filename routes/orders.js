2afa466eb907b587acd7d9ede588aa9b0a943cb7
test <test@test.com>
2022-01-11 14:34:46 +0530

    orders find all & by id.

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Order = require('./../models/order');
 const OrderItem = require('./../models/order-item');
 
@@ -14,12 +15,86 @@ const OrderItem = require('./../models/order-item');
   Desc: get all orders
 */
 router.get('/', (req, res) => {
-  
-  return res.status(200).json({
-    status: true,
-    code: 200,
-    message: 'API orders working.'
-  });
+
+  // read all order from orders collections
+  Order.find()
+    .populate('user', 'name email')
+    .populate({ path: 'orderItems', populate: { path: 'product', populate: 'category' } })
+    .sort({ 'dateOrdered': -1 })
+    .then(orders => {
+      // success response
+      return res.status(200).json({
+        status: true,
+        code: 200,
+        message: 'All order list.',
+        orders: orders
+      });
+    }).catch(error => {
+      // error response
+      return res.status(502).json({
+        status: false,
+        code: 502,
+        message: 'Database error to get all the orders.',
+        error: error
+      });
+    });
+
+});
+
+
+/*
+  URL: api/v1/orders/:id
+  Method: GET
+  Desc: get order by id
+*/
+router.get('/:id', (req, res) => {
+
+  const orderID = req.params.id;
+
+  // validate order id
+  if (!mongoose.isValidObjectId(orderID)) {
+    // error response
+    return res.status(422).json({
+      status: false,
+      code: 422,
+      message: 'Order id is not valid.'
+    });
+  }
+
+
+  // read order by id from orders collections
+  Order.findById(orderID)
+    .populate('user', 'name email')
+    .populate({ path: 'orderItems', populate: { path: 'product', populate: 'category' } })
+    .then(order => {
+
+      // if order not exists
+      if (!order) {
+        // success response
+        return res.status(404).json({
+          status: false,
+          code: 404,
+          message: 'Order not exists.',
+        });
+      }
+
+      // success response
+      return res.status(200).json({
+        status: true,
+        code: 200,
+        message: 'Order by id retreived.',
+        order: order
+      });
+    }).catch(error => {
+      // error response
+      return res.status(502).json({
+        status: false,
+        code: 502,
+        message: 'Database error to get order by id.',
+        error: error
+      });
+    });
+
 });
 
 
